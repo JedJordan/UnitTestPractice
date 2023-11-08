b1e28a1bb0576cdfe3d7cf851cc579bbf1263532
JedJordan <shikirawr@gmail.com>
2023-11-08 11:46:35 -0800

    added mixed case check function and tests

--- Password.cpp
+++ Password.cpp
@@ -19,3 +19,57 @@ int Password::count_leading_characters(string phrase){
   }
   return repetition;
 }
+
+bool Password::has_mixed_case(string phrase){
+  int upper = 0;
+  int lower = 0;
+  for(int i=0;i<phrase.size();i++){
+    if(phrase[i] >= 'A' && phrase[i] <= 'Z'){
+      upper = 1;
+    }
+  }
+  for(int i=0;i<phrase.size();i++){
+    if(phrase[i] >= 'a' && phrase[i] <= 'z'){
+      lower = 1;
+    }
+  }
+  if(upper == 1 and lower == 1){
+    return true;
+  }
+  return false;
+}
+/* kevin's implementation
+bool Password::has_mixed_case(string pass){
+  bool lower = false;
+  bool upper = false;
+  for(int i = 0; i < pass.size(); i++){
+    if( pass[i] >= 'A' && pass[i] <= 'Z' ){
+      upper = true;
+    }
+    else if( pass[i] < 'z'){
+      lower = true;
+    }
+  }
+  return upper && lower;
+/
+
+//anthony's implementation
+bool Password::has_mixed_case(string phrase){
+  bool upper = false;
+  bool lower = false;
+  int index = 0;
+
+  while( index < phrase.length() ){
+    if(isupper(phrase[index])){
+      upper = true;
+    }
+    else if(islower(phrase[index])){
+      lower = true;
+    }
+    index++;
+  }
+  if(upper == true && lower == true){
+    return true;
+  }
+  return false;
+}*/
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -5,22 +5,92 @@
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PracticeTest : public ::testing::Test
+class PasswordTest : public::testing::Test
 {
 	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
+		PasswordTest(){} //constructor runs before each test
+		virtual ~PasswordTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, smoke_test)
-{
-    ASSERT_TRUE( 1 == 1 );
-}
 TEST(PasswordTest, single_letter_password)
 {
     Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1,actual);
+}
+
+TEST(PasswordTest, case_sensitive_password)
+{
+    Password my_password;
+	int actual = my_password.count_leading_characters("ZZZzzz");
+	ASSERT_EQ(3,actual);
+}
+
+TEST(PasswordTest, leading_characters_only)
+{
+    Password my_password;
+	int actual = my_password.count_leading_characters("Zoom");
+	ASSERT_EQ(1,actual);
+}
+
+TEST(PasswordTest, special_character_password)
+{
+    Password my_password;
+	int actual = my_password.count_leading_characters("****hehe****");
+	ASSERT_EQ(4,actual);
+}
+
+TEST(PasswordTest,space_password)
+{
+    Password my_password;
+	int actual = my_password.count_leading_characters("   ");
+	ASSERT_EQ(3,actual);
+}
+
+TEST(PasswordTest, number_password)
+{
+    Password my_password;
+	int actual = my_password.count_leading_characters("1123");
+	ASSERT_EQ(2,actual);
+}
+
+TEST(PasswordTest, mixed_case_password)
+{
+    Password my_password;
+        int actual = my_password.has_mixed_case("aaaBBB");
+	ASSERT_EQ(1,actual);
+}
+
+TEST(PasswordTest, mixed_case_password2)
+{
+    Password my_password;
+    int actual = my_password.has_mixed_case("BBBaaa");
+    ASSERT_EQ(1,actual);
+}
+
+TEST(PasswordTest, mixed_case_password3)
+{
+    Password my_password;
+    int actual = my_password.has_mixed_case("aBaBaB");
+    ASSERT_EQ(1,actual);
+}
+TEST(PasswordTest, empty_password)
+{
+    Password my_password;
+    int actual = my_password.has_mixed_case("");
+    ASSERT_EQ(0,actual);
+}
+TEST(PasswordTest, spaces_before_mixed_password)
+{
+    Password my_password;
+    int actual = my_password.has_mixed_case("  Za");
+    ASSERT_EQ(1,actual);
+}
+TEST(PasswordTest, numbers_ascii_password4)
+{
+    Password my_password;
+    int actual = my_password.has_mixed_case("!@#b$%^&*(Z)");
+    ASSERT_EQ(1,actual);
 }
